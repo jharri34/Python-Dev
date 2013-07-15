ac47a470dd4d29fb6f9c8071a20f5584c7b728ea
jharri34 <jharri34@uab.edu>
2013-07-15 18:54:32 -0500

    delete(tested and working) inorder preorder and postorder all implemented several helper functions added to both the BST class and BSTNode class currently testing traversal methods

--- bst.py
+++ bst.py
@@ -1,45 +1,37 @@
 class BSTnode(object):
-
-    def __init__(self, t):
+    """
+Representation of a node in a binary search tree.
+Has a left child, right child, and key value, and stores its subtree size.
+"""
+    def __init__(self, parent, t):
         """Create a new leaf with key t."""
         self.key = t
-        self.disconnect()
-    def disconnect(self):
-        """sets all of the node references to None"""
+        self.parent = parent
         self.left = None
         self.right = None
-        self.parent = None
+        self.size = 1
+        
+    def update_stats(self):
+        """Updates this node's size based on its children's sizes."""
+        self.size = (0 if self.left is None else self.left.size) + (0 if self.right is None else self.right.size) 
 
-class BST(object):
-    """init root to None only called once"""
-    def __init__(self):
-        self.root = None
-
-    def insert(self, t):
-        """Insert key t into this BST, modifying it in-place."""
-        new = BSTnode(t) #defines a new node in the binary tree
-        if self.root is None:   #sets the root node
-            self.root = new
+    def insert(self, t, NodeType):
+        """Insert key t into the subtree rooted at this node (updating subtree size)."""
+        self.size += 1
+        if t < self.key:
+            if self.left is None:
+                self.left = NodeType(self, t)                
+                return self.left
+            else:
+                return self.left.insert(t, NodeType)
         else:
-            node = self.root
-            while True:
-                if t < node.key:
-                    # Go left
-                    if node.left is None:
-                        node.left = new
-                        new.parent = node
-                        break 
-                    node = node.left
-                else:
-                    # Go right
-                    if node.right is None:
-                        node.right = new
-                        new.parent = node
-                        break
-                    node = node.right
-        return new
+            if self.right is None:
+                self.right = NodeType(self, t)   
+                return self.right
+            else:
+                return self.right.insert(t, NodeType)
 
-     def find(self, t):
+    def find(self, t):
         """Return the node for key t if it is in this tree, or None otherwise."""
         if t == self.key:
             return self
@@ -53,6 +45,24 @@ class BST(object):
                 return None
             else:
                 return self.right.find(t)
+
+            
+    def minimum(self):
+        """Returns the node with the smallest key in the subtree rooted by this node."""
+        current = self
+        while current.left is not None:
+            current = current.left
+        return current
+        
+
+    def successor(self):
+        """Returns the node with the smallest key larger than this node's key, or None if this has the largest key in the tree."""
+        if self.right is not None:
+            return self.right.minimum()
+        current = self
+        while current.parent is not None and current.parent.right is current:
+            current = current.parent
+        return current.parent
 
     def delete(self):
         """"Delete this node from the tree."""
@@ -73,37 +83,71 @@ class BST(object):
         else:
             s = self.successor()
             self.key, s.key = s.key, self.key
-            return s.delete()   
+            return s.delete()        
+        
+    def check(self, lokey, hikey):
+        """Checks that the subtree rooted at t is a valid BST and all keys are between (lokey, hikey)."""
+        if lokey is not None and self.key <= lokey:
+            raise "BST RI violation"
+        if hikey is not None and self.key >= hikey:
+            raise "BST RI violation"
+        if self.left is not None:
+            if self.left.parent is not self:
+                raise "BST RI violation"
+            self.left.check(lokey, self.key)
+        if self.right is not None:
+            if self.right.parent is not self:
+                raise "BST RI violation"
+            self.right.check(self.key, hikey)
+        if self.size != 1 + (0 if self.left is None else self.left.size) + (0 if self.right is None else self.right.size):
+            raise "BST RI violation"
+            
+    def __repr__(self):
+        return "<BST Node, key:" + str(self.key) + ">"
 
-    def inorder(self):
-        self.left.inorder()
-        #visit
-        self.right.inorder()
+class BST(object):
+    """
+Simple binary search tree implementation, augmented with subtree sizes.
+This BST supports insert, find, and delete-min operations.
+Each tree contains some (possibly 0) BSTnode objects, representing nodes,
+and a pointer to the root.
+"""
 
-    def preorder(self):
-        #visit
-        self.left.inorder()
-        self.right.inorder()
+    def __init__(self, NodeType=BSTnode):
+        self.root = None
+        self.NodeType = NodeType
+        self.psroot = self.NodeType(None, None)
+    
+    def reroot(self):
+        self.root = self.psroot.left
 
-    def postorder(self):
-        self.left.inorder()
-        self.right.inorder()
-        #visit
-
-        pass
+    def insert(self, t):
+        """Insert key t into this BST, modifying it in-place."""
+        if self.root is None:
+            self.psroot.left = self.NodeType(self.psroot, t)
+            self.reroot()
+            return self.root
+        else:
+            return self.root.insert(t, self.NodeType)
+        
     def find(self, t):
         """Return the node for key t if is in the tree, or None otherwise."""
-        node = self.root
-        while node is not None:
-            if t == node.key:
-                return node
-            elif t < node.key:
-                node = node.left
-            else:
-                node = node.right
-        return None
+        if self.root is None:
+            return None
+        else:
+            return self.root.find(t)      
+        
+    def delete(self, t):
+        """Delete the node for key t if it is in the tree."""
+        node = self.find(t)
+        deleted = self.root.delete()
+        self.reroot()
+        return deleted
 
-
+    def check(self):
+        if self.root is not None:
+            self.root.check(None, None)
+            
     def __str__(self):
         if self.root is None: return '<empty tree>'
         def recurse(node):
@@ -134,7 +178,6 @@ class BST(object):
         return '\n'.join(recurse(self.root) [0])
 
 
-
 def test(args=None, BSTtype=BST):
     import random, sys
     if not args:
@@ -144,7 +187,7 @@ def test(args=None, BSTtype=BST):
               sys.argv[0]
         sys.exit()
     elif len(args) == 1:
-        items = (random.randrange(100) for i in xrange(int(args[0])))
+        items = [random.randrange(100) for i in xrange(int(args[0]))]
     else:
         items = [int(i) for i in args]
 
@@ -153,6 +196,11 @@ def test(args=None, BSTtype=BST):
     for item in items:
         tree.insert(item)
         print
+        print tree
+    print "Delete"
+    for _ in range(len(items)):
+        tree.delete(random.choice(items))
+        print 
         print tree
 
 if __name__ == '__main__': test()
